a413ec66884b988ad5fcebe084d58763c2c1fcd8
Travis Chambers <travis.chambers@numbersstation.ai>
2024-10-20 06:27:39 -1000

    feat: support local or centralized tracing
    If the .env file contains `ENV=local`, set up tracing
    to stdout instead of sending to Honeycomb.

--- src/main.rs
+++ src/main.rs
@@ -16,55 +16,66 @@ async fn main() {
     use std::time::Duration;
     use tracing::info;
     use tracing_subscriber;
-    use tracing_subscriber::layer::SubscriberExt as _;
     use tracing_subscriber::EnvFilter;
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
     dotenv().ok();
 
+    let environment = env::var("ENV").expect("ENV not set");
+    if environment == "local" {
+        // TODO
+        tracing_subscriber::registry()
+            .with(
+                EnvFilter::try_from_default_env()
+                    .unwrap_or_else(|_| "libbyreads_rs=debug,tower_http=debug,axum=trace".into()),
+            )
+            .with(tracing_subscriber::fmt::layer())
+            .init();
+    } else {
+        let export_config = opentelemetry_otlp::ExportConfig {
+            endpoint: env::var("HONEYCOMB_LOG_API_ENDPOINT")
+                .expect("HONEYCOMB_LOG_API_ENDPOINT not set"),
+            protocol: opentelemetry_otlp::Protocol::HttpBinary,
+            timeout: Duration::from_secs(3),
+        };
+        let log_exporter = opentelemetry_otlp::new_exporter()
+            .http()
+            .with_export_config(export_config)
+            .with_headers({
+                let mut headers = std::collections::HashMap::new();
+                headers.insert(
+                    "x-honeycomb-team".to_string(),
+                    env::var("HONEYCOMB_API_KEY").expect("HONEYCOMB_API_KEY not set"),
+                );
+                headers.insert(
+                    "x-honeycomb-dataset".to_string(),
+                    env::var("HONEYCOMB_DATASET").expect("HONEYCOMB_DATASET not set"),
+                );
+                headers
+            })
+            .build_log_exporter()
+            .unwrap();
+        let resource = Resource::new(vec![
+            KeyValue::new("service.name", "libbyreads"),
+            KeyValue::new("service.version", "0.1.0"),
+        ]);
+        let logger_provider = LoggerProvider::builder()
+            .with_batch_exporter(log_exporter, opentelemetry_sdk::runtime::Tokio)
+            .with_resource(resource)
+            .build();
+
+        let logger_layer = layer::OpenTelemetryTracingBridge::new(&logger_provider);
+        let env_filter_layer = EnvFilter::try_from_default_env()
+            .or_else(|_| EnvFilter::try_new("info"))
+            .unwrap();
+
+        let subscriber = tracing_subscriber::registry().with(env_filter_layer);
+
+        tracing::subscriber::set_global_default(subscriber.with(logger_layer)).unwrap();
+    }
+
     console_error_panic_hook::set_once();
 
-    // tracing_subscriber::fmt::init();
-
-    let export_config = opentelemetry_otlp::ExportConfig {
-        endpoint: env::var("HONEYCOMB_LOG_API_ENDPOINT")
-            .expect("HONEYCOMB_LOG_API_ENDPOINT not set"),
-        protocol: opentelemetry_otlp::Protocol::HttpBinary,
-        timeout: Duration::from_secs(3),
-    };
-    let log_exporter = opentelemetry_otlp::new_exporter()
-        .http()
-        .with_export_config(export_config)
-        .with_headers({
-            let mut headers = std::collections::HashMap::new();
-            headers.insert(
-                "x-honeycomb-team".to_string(),
-                env::var("HONEYCOMB_API_KEY").expect("HONEYCOMB_API_KEY not set"),
-            );
-            headers.insert(
-                "x-honeycomb-dataset".to_string(),
-                env::var("HONEYCOMB_DATASET").expect("HONEYCOMB_DATASET not set"),
-            );
-            headers
-        })
-        .build_log_exporter()
-        .unwrap();
-    let resource = Resource::new(vec![
-        KeyValue::new("service.name", "libbyreads"),
-        KeyValue::new("service.version", "0.1.0"),
-    ]);
-    let logger_provider = LoggerProvider::builder()
-        .with_batch_exporter(log_exporter, opentelemetry_sdk::runtime::Tokio)
-        .with_resource(resource)
-        .build();
-
-    let logger_layer = layer::OpenTelemetryTracingBridge::new(&logger_provider);
-    let env_filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))
-        .unwrap();
-
-    let subscriber = tracing_subscriber::registry().with(env_filter_layer);
-
-    tracing::subscriber::set_global_default(subscriber.with(logger_layer)).unwrap();
     info!("Starting server");
     // Setting get_configuration(None) means we'll be using cargo-leptos's env values
     // For deployment these variables are:
